198885f3a70914db18fea009ca16c74201b4c1dd
Oscar Rodriguez <mld.oscar@yahoo.com>
2017-10-03 21:28:36 -0600

    Made some code scope refactoring

--- lib/bcn_ni.rb
+++ lib/bcn_ni.rb
@@ -1,23 +1,14 @@
 require 'uri'
 require 'net/http'
 require 'net/https'
+require 'active_support/core_ext/hash'
 require 'json'
 
 module BcnNi
 
   def self.exchange_day(year, month, day)
-    body = <<EOF
-<?xml version="1.0" encoding="utf-8"?>
-<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
-  <soap:Body>
-    <RecuperaTC_Dia xmlns="http://servicios.bcn.gob.ni/">
-      <Ano>#{year}</Ano>
-      <Mes>#{month}</Mes>
-      <Dia>#{day}</Dia>
-    </RecuperaTC_Dia>
-  </soap:Body>
-</soap:Envelope>
-EOF
+    # Build body through a XML envelope
+    body = self.bcn_envelope(self.exchange_day_letter(year, month, day))
 
     json_response = self.do_request(body)
 
@@ -30,17 +21,8 @@ EOF
   end
 
   def self.exchange_month(year, month)
-    body = <<EOF
-<?xml version="1.0" encoding="utf-8"?>
-<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
-  <soap:Body>
-    <RecuperaTC_Mes xmlns="http://servicios.bcn.gob.ni/">
-      <Ano>#{year}</Ano>
-      <Mes>#{month}</Mes>
-    </RecuperaTC_Mes>
-  </soap:Body>
-</soap:Envelope>
-EOF
+    # Build body through a XML envelope
+    body = self.bcn_envelope(self.exchange_month_letter(year, month))
 
     json_response = self.do_request(body)
 
@@ -60,32 +42,66 @@ EOF
 
   end
 
-  def self.do_request(body)
-    # see 'https://servicios.bcn.gob.ni/Tc_Servicio/ServicioTC.asmx'
-    # for more info about how to build a SOAP RAW request
+  private
+    def self.do_request(body)
+      # see 'https://servicios.bcn.gob.ni/Tc_Servicio/ServicioTC.asmx'
+      # for more info about how to build a RAW SOAP request
 
-    # Parse the URI
-    uri = URI.parse('https://servicios.bcn.gob.ni/Tc_Servicio/ServicioTC.asmx?WSDL')
+      # Parse the URI
+      uri = URI.parse('https://servicios.bcn.gob.ni/Tc_Servicio/ServicioTC.asmx?WSDL')
 
-    # Create protocol to the URI
-    https = Net::HTTP.new(uri.host, uri.port)
+      # Create protocol to the URI
+      https = Net::HTTP.new(uri.host, uri.port)
 
-    https.use_ssl = true
+      https.use_ssl = true
 
-    # Create a new POST request as XML content type
-    req = Net::HTTP::Post.new(uri.path, initheader = {'Content-Type' => 'text/xml'})
+      # Create a new POST request as XML content type
+      req = Net::HTTP::Post.new(uri.path, initheader = {'Content-Type' => 'text/xml'})
 
-    # Set the request body as a RAW SOAP XML request
-    req.body = body
+      # Set the request body as a RAW SOAP XML request
+      req.body = body
 
-    # Process the request
-    res = https.request(req)
+      # Process the request
+      res = https.request(req)
 
-    # Get the XML response
-    xml_response = res.body
+      # Get the XML response
+      xml_response = res.body
 
-    # Parse to a JSON hash
-    return Hash.from_xml(xml_response)
-  end
+      # Parse to a JSON hash
+      return Hash.from_xml(xml_response)
+    end
+
+    def self.bcn_envelope(letter)
+      envelope = <<EOF
+<?xml version="1.0" encoding="utf-8"?>
+<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+  <soap:Body>
+    #{letter}
+  </soap:Body>
+</soap:Envelope>
+EOF
+      return envelope
+    end
+
+    def self.exchange_day_letter(year, month, day)
+      letter = <<EOF
+<RecuperaTC_Dia xmlns="http://servicios.bcn.gob.ni/">
+  <Ano>#{year}</Ano>
+  <Mes>#{month}</Mes>
+  <Dia>#{day}</Dia>
+</RecuperaTC_Dia>
+EOF
+      return letter
+    end
+
+    def self.exchange_month_letter(year, month)
+      letter = <<EOF
+<RecuperaTC_Mes xmlns="http://servicios.bcn.gob.ni/">
+  <Ano>#{year}</Ano>
+  <Mes>#{month}</Mes>
+</RecuperaTC_Mes>
+EOF
+      return letter
+    end
 
 end
