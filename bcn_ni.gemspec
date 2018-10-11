608f7cde8982a5226070bd7331f87623cd02bffb
Alexander Cruz <cnoealexander@gmail.com>
2018-10-10 21:04:45 -0600

    Refactored and simplified requests to webservice

--- bcn_ni.gemspec
+++ bcn_ni.gemspec
@@ -17,6 +17,7 @@ Gem::Specification.new do |s|
   s.files = Dir["{app,config,db,lib}/**/*", "MIT-LICENSE", "Rakefile", "README.md"]
 
   s.add_dependency "activesupport", "~> 5.1", ">= 5.1.3"
+  s.add_dependency 'savon', '~> 2.12'
 
   s.add_development_dependency "rails", "~> 5.1", ">= 5.1.3"
   s.add_development_dependency "sqlite3", "~> 1.3", ">= 1.3.12"
--- lib/bcn_ni.rb
+++ lib/bcn_ni.rb
@@ -1,41 +1,30 @@
-require 'uri'
-require 'net/http'
-require 'net/https'
 require 'active_support/core_ext/hash'
-require 'json'
+require 'savon'
 
 module BcnNi
 
   def self.exchange_day(year, month, day)
     # Build body through a XML envelope
-    body = self.bcn_envelope(self.exchange_day_letter(year, month, day))
+    body = self.post_webservice(year, month, day)
 
-    json_response = self.do_request(body)
-
-    # Get the result value
-    value_result = json_response['Envelope']['Body']['RecuperaTC_DiaResponse']['RecuperaTC_DiaResult']
-
-    # Parse the result value and finally return it
-    return value_result.to_f
-
+    # Return exchange rate value
+    body[:recupera_tc_dia_response][:recupera_tc_dia_result].to_f
   end
 
   def self.exchange_month(year, month)
     # Build body through a XML envelope
-    body = self.bcn_envelope(self.exchange_month_letter(year, month))
-
-    json_response = self.do_request(body)
+    body = self.post_webservice(year, month)
 
     # Get the result array
-    exchange_table = json_response['Envelope']['Body']['RecuperaTC_MesResponse']['RecuperaTC_MesResult']['Detalle_TC']['Tc']
+    exchange_rates = body[:recupera_tc_mes_response][:recupera_tc_mes_result][:detalle_tc][:tc]
 
-    unless exchange_table.nil?
+    if exchange_rates.any?
       # Parse the table to a custom and better JSON
       # The format example will be: {date: as Date, value: as Float}
-      parsed_table = exchange_table.map{|x| {date: Date.parse(x['Fecha']), value: x['Valor'].to_f} }
+      parsed_table = exchange_rates.map { |e| { date: e[:fecha], value: e[:valor].to_f } }
 
       # Sort the parsed table and finally return it
-      return parsed_table.sort_by {|x| x[:date]}
+      return parsed_table.sort_by { |e| e[:date] }
     else
       return []
     end
@@ -43,65 +32,18 @@ module BcnNi
   end
 
   private
-    def self.do_request(body)
-      # see 'https://servicios.bcn.gob.ni/Tc_Servicio/ServicioTC.asmx'
-      # for more info about how to build a RAW SOAP request
 
-      # Parse the URI
-      uri = URI.parse('https://servicios.bcn.gob.ni/Tc_Servicio/ServicioTC.asmx?WSDL')
+  def self.post_webservice(year, month, day = nil)
+    date = { Ano: year, Mes: month }
+    date[:Dia] = day if day.present?
 
-      # Create protocol to the URI
-      https = Net::HTTP.new(uri.host, uri.port)
-
-      https.use_ssl = true
-
-      # Create a new POST request as XML content type
-      req = Net::HTTP::Post.new(uri.path, initheader = {'Content-Type' => 'text/xml'})
-
-      # Set the request body as a RAW SOAP XML request
-      req.body = body
-
-      # Process the request
-      res = https.request(req)
-
-      # Get the XML response
-      xml_response = res.body
-
-      # Parse to a JSON hash
-      return Hash.from_xml(xml_response)
+    request = Savon.client do
+      wsdl 'https://servicios.bcn.gob.ni/Tc_Servicio/ServicioTC.asmx?WSDL'
+      convert_request_keys_to :camelcase
     end
 
-    def self.bcn_envelope(letter)
-      envelope = <<EOF
-<?xml version="1.0" encoding="utf-8"?>
-<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
-  <soap:Body>
-    #{letter}
-  </soap:Body>
-</soap:Envelope>
-EOF
-      return envelope
-    end
+    exchange_rates = day.nil? ? :recupera_tc_mes : :recupera_tc_dia
 
-    def self.exchange_day_letter(year, month, day)
-      letter = <<EOF
-<RecuperaTC_Dia xmlns="http://servicios.bcn.gob.ni/">
-  <Ano>#{year}</Ano>
-  <Mes>#{month}</Mes>
-  <Dia>#{day}</Dia>
-</RecuperaTC_Dia>
-EOF
-      return letter
-    end
-
-    def self.exchange_month_letter(year, month)
-      letter = <<EOF
-<RecuperaTC_Mes xmlns="http://servicios.bcn.gob.ni/">
-  <Ano>#{year}</Ano>
-  <Mes>#{month}</Mes>
-</RecuperaTC_Mes>
-EOF
-      return letter
-    end
-
+    request.call(exchange_rates, message: date).body
+  end
 end
